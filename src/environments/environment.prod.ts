e5c7169c8572e9abb0575d59083eb95529e8f49d
Andrey Chibisov <a.chibisov@corp.mail.ru>
2018-11-15 10:20:55 +0300

    Production env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: `http://159.69.199.206/api/v1/free`,
-  apiMediaUrl: `http://159.69.199.206`,
-}
+  apiUrl: `http://pipeline.live/api/v1/free`,
+  apiMediaUrl: `http://pipeline.live`,
+};
