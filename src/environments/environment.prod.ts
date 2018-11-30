1e5e3d19da2fff71de5d86b2fd95e5191b9e923d
Andrey Chibisov <a.chibisov@corp.mail.ru>
2018-11-30 16:06:58 +0300

    API Migrated from http to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiHost: `http://pipeline.live`,
-  apiUrl: `http://pipeline.live/api`,
+  apiHost: `https://pipeline.live`,
+  apiUrl: `https://pipeline.live/api`,
   trackers: {
     googleAnalyticsUA: ``,
   }
